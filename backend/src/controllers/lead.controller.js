dcbd8c97a3c23fc0c1bdc4c9db21e5f8345f328f
sandeepleadmastersai <sandeepleadmastersai@gmail.com>
2026-02-04 16:29:13 +0530

    feat: add updateLeadStatus in lead controller and add updateLeadStatus route

--- backend/src/controllers/lead.controller.js
+++ backend/src/controllers/lead.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Lead from "../models/lead.model.js";
 
 
@@ -33,6 +34,38 @@ export const getLeads = async (req, res) => {
     try {
         const leads = await Lead.find().sort({ createdAt: -1 });
         return res.status(200).json(leads);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+export const updateLeadStatus = async (req, res) => {
+    try {
+        const { status } = req.body;
+
+        if (!["new", "contacted", "closed"].includes(status)) {
+            res.status(400).json({ message: "Invalid Lead Source"});
+        } 
+
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            res.status(400).json({ message: "Lead ID not found"});
+        }
+
+        const lead = await Lead.findByIdAndUpdate(
+            id,
+            { status: req.body.status },
+            { new: true}, 
+        );
+
+        if (!lead) {
+            res.status(404).json({ message: "Lead not found" });
+        }
+
+        return res.status(200).json(lead);
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: error.message });
--- backend/src/routes/lead.route.js
+++ backend/src/routes/lead.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createLead, getLeads } from "../controllers/lead.controller.js";
+import { createLead, getLeads, updateLeadStatus } from "../controllers/lead.controller.js";
 
 const router = express.Router();
 
 router.post("/", createLead);
 router.get("/", getLeads);
+router.patch("/:id/status", updateLeadStatus);
 
 export default router;
