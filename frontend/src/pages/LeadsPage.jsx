00ce81870457ae9ad1ff8e6563c14d709c10a076
sandeepleadmastersai <sandeepleadmastersai@gmail.com>
2026-02-04 17:53:25 +0530

    feat: add loading state

--- frontend/src/pages/LeadsPage.jsx
+++ frontend/src/pages/LeadsPage.jsx
@@ -3,14 +3,20 @@ import { API } from "../api/axios.js";
 
 function LeadsPage() {
     const [leads, setLeads] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const fetchLeads = async () => {
         try {
+            setLoading(true);
             const response = await API.get("/leads");
             const leads = response.data;
 
             if (leads && leads.length > 0) {
                 setLeads(leads);
+                setLoading(false);
+            } else {
+                setLeads([]);
+                setLoading(false);
             }
         } catch (error) {
             console.log(error);
@@ -28,11 +34,38 @@ function LeadsPage() {
     return (
         <div>
             <h1>Leads Page</h1>
-            <ul>
-                {
-                    leads.map((leadItem) => <li>{leadItem.name}</li>)
-                }
-            </ul>
+            {
+                loading ? (
+                    <p>Loading Leads...</p>
+                ) : (
+                    <ul>
+                        {
+                            leads.map((leadItem) => <li>{leadItem.name}</li>)
+                        }
+                    </ul>
+                )
+            }
+            {/* <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Mark</td>
+                        <td>1</td>
+                        <td>new</td>
+                    </tr>
+                    <tr>
+                        <td>Jacob</td>
+                        <td>2</td>
+                        <td>new</td>
+                    </tr>
+                </tbody>
+            </table> */}
         </div>
     );
 }
